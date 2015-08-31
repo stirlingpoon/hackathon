a9b4a0e24777a9174ab0e1e09c15cfa411104076
bobbylht3 <bobbylht3@yahoo.com.hk>
2015-08-31 16:48:42 +0800

    Created Post API with Mock and profileInfo Service

---
+++ app/services/api/api.js
@@ -0,0 +1,28 @@
+App.factory('api', ['$scope', '$http', function apiFactory($scope, $http) {
+  var api = {};
+  
+  api.post = function(url, data){
+	 
+	var deferred = $q.defer();
+	$http.post(url, data).
+		then(function(response) {
+			deferred.resolve(response);
+		}, function(response) {
+			deferred.reject('POST request to '+url+' failed with error: '+response);
+		});
+	return deferred.promise;
+  };
+  
+  api.mockPost = function(url, data, mockResponse, mockErrorResponse, isMockingError){
+	 
+	var deferred = $q.defer();
+	
+	if(isMockingError)
+		deferred.reject('POST request to '+url+' failed with error: '+mockErrorResponse);
+	else
+		deferred.resolve(mockResponse);
+	return deferred.promise;
+  };
+  
+  return api;
+}]);
---
+++ app/services/profileInfo/profileInfo.js
@@ -0,0 +1,21 @@
+App.factory('profileInfo', ['$scope', 'api', function profileInfoFactory($scope, $api) {
+  var profileInfo = {};
+  
+  profileInfo.getInfo = function(email)
+  {
+	  var deferred = $q.defer();
+	  
+	  $api.mockPost('/profileInfo', {email: 'mock@credit-suisse.com'}, {
+		  firstName: 'Mock',
+		  lastName: 'user',
+		  skills: ['C++', 'Java', 'XML']
+	  }, 'Mock Error', false)
+	  .then(function(response){
+		  deferred.resolve(response);
+	  }, function(response){
+		  deferred.reject(response);
+	  });
+	  
+	  return deferred.promise;
+  }
+}]);
