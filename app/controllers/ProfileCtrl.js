d7ef94dada259dcfdb81e607fcc6ede01c904a10
Poon Stirling Yeu <poonyeu@gmail.com>
2015-09-01 13:47:55 +0800

    Added autocomplete to add skill

--- app/controllers/ProfileCtrl.js
+++ app/controllers/ProfileCtrl.js
@@ -3,6 +3,7 @@ import _ from 'lodash';
 
 export default ['$scope', '$routeParams', class ProfileCtrl {
     constructor($scope, $routeParams) {
+        this.$scope = $scope;
         this.$routeParams = $routeParams;
         console.log('profile id', $routeParams.profileId);
         ProfileInfo.getInfo($routeParams.profileId).then(({firstName, lastName, name, email, skills}) => {
@@ -14,6 +15,26 @@ export default ['$scope', '$routeParams', class ProfileCtrl {
         });
     }
 
+    querySearch(query) {
+        return ProfileInfo.getSkills(query);
+    }
+
+    searchTextChange(text) {
+        console.log('Text changed to ' + text);
+    }
+
+    selectedItemChange(item) {
+        console.log('Item changed to', item);
+        if (item !== undefined && this.$scope.skills.filter(({name}) => name === item.display || name === item.searchKey).length === 0) {
+            this.$scope.skills.push({
+                name: item.display,
+                endorsedBy: []
+            });
+            this.searchText = null;
+        }
+    }
+
+    /*
     newSkill(skill) {
         return {
             name: skill,
@@ -21,12 +42,12 @@ export default ['$scope', '$routeParams', class ProfileCtrl {
             endorsedBy: []
         }
     }
+    */
 
     endorseSkill(skill) {
         console.log('endorseSkill', skill);
         var id = parseInt(this.$routeParams.profileId, 10);
         if (skill.endorsedBy.indexOf(id) === -1) {
-            skill.rank++;
             skill.endorsedBy.push(id);
             ProfileInfo.endorseSkill(id, skill);
         }
