9349893a0db17ea3f37a9c85680021ecb69f3b9a
bobbylht3 <bobbylht3@yahoo.com.hk>
2015-09-01 11:09:42 +0800

    Styling on the profile info page

--- app/controllers/ProfileCtrl.js
+++ app/controllers/ProfileCtrl.js
@@ -3,10 +3,11 @@ import _ from 'lodash';
 
 export default ['$scope', class ProfileCtrl {
     constructor($scope) {
-        getInfo().then(({firstName, lastName, skills}) => {
+        getInfo().then(({firstName, lastName, email, skills}) => {
             $scope.name = firstName + ' ' + lastName;
             $scope.role = 'AVP';
             $scope.face = 'app/assets/spencer.jpg';
+			$scope.email = email;
             $scope.skills = skills;
         });
     }
--- app/services/profileInfo/profileInfo.js
+++ app/services/profileInfo/profileInfo.js
@@ -4,6 +4,7 @@ export const getInfo = (email) =>
 	mockPost('/profileInfo', {email: 'mock@credit-suisse.com'}, {
 		  firstName: 'Spencer',
 		  lastName: 'Goh',
+		  email: 'spencer.goh@credit-suisse.com',
 		  skills: [{name: 'C++', rank: 1}, {name: 'Java', rank: 3}, {name: 'XML', rank: 2}]
 	  }, 'Mock Error', false);
 
